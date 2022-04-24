c122f37b2e84a5c9efcc953ba487103534204846
Laura <lauramjohnson@me.com>
2022-04-24 11:57:33 -0500

    Initial setup

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Laura Johnson on 4/24/22.
+//
+
+import SwiftUI
+
+// MARK:  - formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK:  - UI
+
+// MARK:  - UX
